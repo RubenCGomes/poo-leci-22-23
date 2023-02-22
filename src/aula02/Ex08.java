c9419734a0a9c5dc60e9f465b6b514abfb2e7f47
RubenCG <rlcg@ua.pt>
2023-02-22 11:42:01 +0000

    Added if statement in aula02/Ex08

--- src/aula02/Ex08.java
+++ src/aula02/Ex08.java
@@ -10,9 +10,14 @@ public class Ex08 {
         c1 = inp.nextDouble();
         System.out.println("Indique o valor do segundo cateto(B): ");
         c2 = inp.nextDouble();
-        h = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
-        System.out.println("O valor da hipotenusa(C) é " + h);
-        ang = Math.asin(c2 / h);
-        System.out.println("O ângulo entre A e C é " + Math.toDegrees(ang) + "º");
+        if (c1 <= 0 || c2 <= 0){
+            System.out.println("Introduziu um valor inválido");
+        }
+        else{
+            h = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+            System.out.println("O valor da hipotenusa(C) é " + h);
+            ang = Math.asin(c2 / h);
+            System.out.println("O ângulo entre A e C é " + Math.toDegrees(ang) + "º");
+        }
     }
 }
