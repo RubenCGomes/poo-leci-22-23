1025d9b5a2a9d5cf8f3e073937184925480b20f9
RubenCG <rlcg@ua.pt>
2023-02-28 22:36:29 +0000

    Added 2 new functions to verify double values

--- src/utils/UserInput.java
+++ src/utils/UserInput.java
@@ -3,7 +3,7 @@ package utils;
 import java.util.Scanner;
 
 public class UserInput {
-    /*For closed intervals*/
+    /*For closed int intervals*/
     public static int readIntClosed(double min, double max, String message){
         System.out.println(message);
         Scanner inp = new Scanner(System.in);
@@ -14,7 +14,8 @@ public class UserInput {
         }
         return number;
     }
-    /*For open intervals*/
+
+    /*For open int intervals*/
     public static int readIntOpen(double min, double max, String message){
         System.out.println(message);
         Scanner inp = new Scanner(System.in);
@@ -22,6 +23,30 @@ public class UserInput {
         while (min >= number || number >= max){
             System.out.println(message);
             number = inp.nextInt();
+        }
+        return number;
+    }
+
+    /*For closed double intervals*/
+    public static double readDoubleClosed(double min, double max, String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        double number = inp.nextDouble();
+        while (min > number || number > max){
+            System.out.println(message);
+            number = inp.nextDouble();
+        }
+        return number;
+    }
+
+    /*For open double intervals*/
+    public static double readDoubleOpen(double min, double max, String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        double number = inp.nextDouble();
+        while (min >= number || number >= max){
+            System.out.println(message);
+            number = inp.nextDouble();
         }
         return number;
     }
