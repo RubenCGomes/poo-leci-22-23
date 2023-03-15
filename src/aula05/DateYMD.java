cdd2892822b0479c1f1cdd59f152547eca30fd3b
RubenCG <rlcg@ua.pt>
2023-03-15 09:08:57 +0000

    aula05 ex01 added

---
+++ src/aula05/DateYMD.java
@@ -0,0 +1,66 @@
+package aula05;
+
+class DateYMD {
+    int days, month, year;
+    public DateYMD(int days, int month, int year){
+        if (valid(days, month, year)){
+            this.days = days; this.month = month; this.year = year;
+        }
+        else System.out.println("Invalid date, try again");
+    }
+
+    public static int monthDays(int month, int year){
+        int days;
+        switch (month) {
+            case 2 -> {
+                if (leapYear(year)) days = 29;
+                else days = 28;
+            }
+            case 4, 6, 9, 11 -> days = 30;
+            default -> days = 31;
+        }
+        return days;
+    }
+
+    public static boolean leapYear(int year){
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+    }
+
+    public static boolean validMonth(int month){
+        return (0 < month && month <= 12);
+    }
+
+    public static boolean valid(int days, int month, int year){
+        if (!validMonth(month)) return false;
+        if (days <= 0) return false;
+        if (month == 2){
+            if (leapYear(year)) return days <= 29;
+            else return days <= 28;
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11) return days <= 30;
+        return days <= 31;
+    }
+
+    public void setDate(int days, int month, int year){
+        if (valid(days, month, year)){
+            this.days = days; this.month = month; this.year = year;
+        }
+        else System.out.println("Invalid date, try again");
+    }
+
+    public int[] getDate(){ return new int[]{this.days, this.month, this.year};}
+
+    public String toString(){return days + "/" + month + "/" + year;}
+
+    public void increment(){
+        if (valid(this.days + 1, this.month, this.year)){++this.days;}
+        else if (this.month + 1 > 12){this.days = 1; this.month = 1; ++this.year;}
+        else {this.days = 1; ++this.month;}
+        }
+
+    public void decrement(){
+        if (valid(this.days - 1, this.month, this.year)){--this.days;}
+        else if (this.month - 1 < 1) {this.days = 31; this.month = 12; --this.year;}
+        else {this.days = monthDays(this.month, this.year); --this.month;}
+    }
+}
---
+++ src/aula05/Ex01.java
@@ -0,0 +1,55 @@
+package aula05;
+
+import utils.*;
+
+public class Ex01 {
+    public static void main(String[] args){
+        DateYMD date = null;
+        while (true){
+            int option = UserInput.readIntClosed(0, 6, "Date operations:\n1 - create new date\n2 - show current date\n3 - set new date\n4 - increment date\n5 - decrement date\n6 - print date\n0 - exit");
+            switch (option) {
+                case 0 -> {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }
+                case 1 -> {
+                    int day = UserInput.readIntClosed(1, 31, "Day: ");
+                    int month = UserInput.readIntClosed(1, 12, "Month: ");
+                    int year = UserInput.readIntClosed(1000, 9999, "Year: ");
+                    if (DateYMD.valid(day, month, year)) {
+                        date = new DateYMD(day, month, year);
+                    } else {
+                        System.out.println("Invalid date, try again");
+                    }
+                }
+                case 2 -> {
+                    assert date != null;
+                    System.out.println("Current date: " + date.getDate()[0] + "/" + date.getDate()[1] + "/" + date.getDate()[2]);
+                }
+                case 3 -> {
+                    assert date != null;
+                    int newDay = UserInput.readIntClosed(1, 31, "Day: ");
+                    int newMonth = UserInput.readIntClosed(1, 12, "Month: ");
+                    int newYear = UserInput.readIntClosed(1000, 9999, "Year: ");
+                    if (DateYMD.valid(newDay, newMonth, newYear)) {
+                        date.setDate(newDay, newMonth, newYear);
+                    } else {
+                        System.out.println("Invalid date, try again");
+                    }
+                }
+                case 4 -> {
+                    assert date != null;
+                    date.increment();
+                }
+                case 5 -> {
+                    assert date != null;
+                    date.decrement();
+                }
+                case 6 -> {
+                    assert date != null;
+                    System.out.println(date);
+                }
+            }
+        }
+    }
+}
