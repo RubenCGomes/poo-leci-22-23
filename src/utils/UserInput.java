0ee9d646f6f06fc5df738521ba5be1d493c9dbd8
RubenCG <rlcg@ua.pt>
2023-03-23 00:37:27 +0000

    Added a few new functions

--- src/utils/UserInput.java
+++ src/utils/UserInput.java
@@ -50,4 +50,56 @@ public class UserInput {
         }
         return number;
     }
+
+    /*Read a email with the following format: <name>@<domain>.<extension>*/
+    public static String readEmail(String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        String email = inp.nextLine();
+        while (!email.matches("^[a-zA-Z_0-9]+@[a-zA-Z]+\\.+[a-zA-Z]+$")){
+            System.out.println("Email inválido");
+            System.out.println(message);
+            email = inp.nextLine();
+        }
+        return email;
+    }
+
+    /*Read a phone number with 9 digits that begins with 9*/
+    public static int readNumber(String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        int number = inp.nextInt();
+        while (!String.valueOf(number).matches("9\\d{8}")){
+            System.out.println("Número inválido");
+            System.out.println(message);
+            number = inp.nextInt();
+        }
+        return number;
+    }
+
+    /*Reads a name*/
+    public static String readName(String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        String name = inp.nextLine();
+        while (!name.matches("[ A-Za-z]+")){
+            System.out.println("Nome inválido");
+            System.out.println(message);
+            name = inp.nextLine();
+        }
+        return name;
+    }
+
+    /*Reads a valid CC*/
+    public static int readCc(String message){
+        System.out.println(message);
+        Scanner inp = new Scanner(System.in);
+        int cc = inp.nextInt();
+        while (!(String.valueOf(cc).length() == 8)){
+            System.out.println("CC inválido");
+            System.out.println(message);
+            cc = inp.nextInt();
+        }
+        return cc;
+    }
 }
