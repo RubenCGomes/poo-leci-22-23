542db21102cb3265a82a7c96d4ba8c42a8da7127
RubenCG <rlcg@ua.pt>
2023-05-21 21:13:48 +0100

    removed unused imports

--- src/aula11/ex03/EnergyUsageReport.java
+++ src/aula11/ex03/EnergyUsageReport.java
@@ -3,7 +3,6 @@ package aula11.ex03;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
--- src/aula12/ex02/SortByInitial.java
+++ src/aula12/ex02/SortByInitial.java
@@ -1,6 +1,5 @@
 package aula12.ex02;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
